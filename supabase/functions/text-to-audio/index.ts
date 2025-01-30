d166a5af16154f7ac3cf55fce9a04c98d5bc210f
kallebysantos <kalleby_santos@hotmail.com>
2025-01-30 10:22:40 +0000

    stamp: using `kokoro.js` to generate audio from text

--- supabase/functions/text-to-audio/index.ts
+++ supabase/functions/text-to-audio/index.ts
@@ -2,47 +2,30 @@
 import "jsr:@supabase/functions-js/edge-runtime.d.ts";
 import {
   PreTrainedTokenizer,
-  RawAudio,
 } from "https://cdn.jsdelivr.net/npm/@huggingface/transformers@3.3.1";
+import { KokoroTTS } from "https://cdn.jsdelivr.net/npm/kokoro-js@1.0.1/+esm";
 
 import InferenceAPI from "inference-api";
-const { Tensor, RawSession } = InferenceAPI;
+const { RawSession } = InferenceAPI;
 
-const STYLE_DIM = 256;
-const SAMPLE_RATE = 24000;
 const MODEL_ID = "onnx-community/Kokoro-82M-ONNX";
 
 const session = await RawSession.fromHuggingFace(MODEL_ID);
+const tokenizer = await loadTokenizer();
+// BUG: KokoroTTS.from_pretrained() can't load tokenizer, so we need to pass manually
+const kokoro = new KokoroTTS(
+  async (...args) => await session.run(...args),
+  tokenizer,
+);
 
-console.log(session);
-/*
-session {
-  id: "6325400502544830693",
-  input: [ "input_ids", "style", "speed" ],
-  output: [ "waveform" ]
-}
-*/
+Deno.serve(async (req) => {
+  const params = new URL(req.url).searchParams;
+  const text = params.get("text");
+  const voice = params.get("voice") ?? "af_bella";
 
-Deno.serve(async (_) => {
-  const tokenizer = await loadTokenizer();
-  const { input_ids } = tokenizer("Hello");
+  const audio = await kokoro.generate(text, { voice });
 
-  // Select voice style based on number of input tokens
-  const num_tokens = Math.max(
-    input_ids.dims.at(-1) - 2, // Without padding;
-    0,
-  );
-
-  const voiceStyle = await loadVoiceStyle(num_tokens);
-
-  const { waveform } = await session.run({
-    input_ids,
-    style: voiceStyle,
-    speed: new Tensor("float32", [1], [1]),
-  });
-
-  const audio = new RawAudio(waveform.data, SAMPLE_RATE);
-  console.log(waveform, audio);
+  console.log("generated", audio);
 
   return new Response(await audio.toBlob(), {
     headers: {
@@ -50,23 +33,6 @@ Deno.serve(async (_) => {
     },
   });
 });
-
-async function loadVoiceStyle(num_tokens: number) {
-  const voice_url =
-    "https://huggingface.co/onnx-community/Kokoro-82M-ONNX/resolve/main/voices/af_bella.bin?download=true";
-
-  const voiceBuffer = await fetch(voice_url).then(async (res) =>
-    await res.arrayBuffer()
-  );
-
-  const offset = num_tokens * STYLE_DIM;
-  const voiceData = new Float32Array(voiceBuffer).slice(
-    offset,
-    offset + STYLE_DIM,
-  );
-
-  return new Tensor("float32", voiceData, [1, STYLE_DIM]);
-}
 
 async function loadTokenizer() {
   // BUG: invalid 'h' not JSON. That's why we need to manually fetch the assets
